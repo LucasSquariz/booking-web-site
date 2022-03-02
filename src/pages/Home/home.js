54e9d7c6036f801feca39b7ef0eb3bbd58f31f3e
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2022-03-02 17:52:12 -0300

    Adicionando cards na home

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,159 @@
+import { Card, CardGroup, Button } from 'react-bootstrap';
 import './home.scss';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-        
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
         </>
     )
 }
