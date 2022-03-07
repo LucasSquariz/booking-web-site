6d8adf059cfee1dcd8d7fe303e5af535843bdd32
Lucas Silveira <85209669+LucasSquariz@users.noreply.github.com>
2022-03-07 18:36:13 -0300

    Iniciando cards

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,4 +1,4 @@
-import { Card, CardGroup, Button } from 'react-bootstrap';
+import { Card, CardGroup, Button, ListGroupItem } from 'react-bootstrap';
 import './home.scss';
 
 function Home() {
@@ -6,20 +6,28 @@ function Home() {
         <>
             <CardGroup>
                 <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Img variant="top" src="https://betaimages.lopes.com.br/realestate/sml/REO297712/5CFACCDFF2BA3BF203708E56FEA3851B.JPG" />
                     <Card.Body>
-                        <Card.Title>Card title</Card.Title>
                         <Card.Text>
+                            <p>Hotel   
+                                <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.27778 0L8.91174 4.83688H14.1994L9.92159 7.82624L11.5555 12.6631L7.27778 9.67376L3.00001 12.6631L4.63397 7.82624L0.3562 4.83688H5.64382L7.27778 0Z" fill="#1DBEB4" /></svg>
+                                <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.27778 0L8.91174 4.83688H14.1994L9.92159 7.82624L11.5555 12.6631L7.27778 9.67376L3.00001 12.6631L4.63397 7.82624L0.3562 4.83688H5.64382L7.27778 0Z" fill="#1DBEB4" /></svg>
+                                <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.27778 0L8.91174 4.83688H14.1994L9.92159 7.82624L11.5555 12.6631L7.27778 9.67376L3.00001 12.6631L4.63397 7.82624L0.3562 4.83688H5.64382L7.27778 0Z" fill="#1DBEB4" /></svg>
+                                <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.27778 0L8.91174 4.83688H14.1994L9.92159 7.82624L11.5555 12.6631L7.27778 9.67376L3.00001 12.6631L4.63397 7.82624L0.3562 4.83688H5.64382L7.27778 0Z" fill="#1DBEB4" /></svg>
+                                <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.27778 0L8.91174 4.83688H14.1994L9.92159 7.82624L11.5555 12.6631L7.27778 9.67376L3.00001 12.6631L4.63397 7.82624L0.3562 4.83688H5.64382L7.27778 0Z" fill="#1DBEB4" /></svg>
+                            </p>
+                        </Card.Text>
+                        <Card.Title>Nome Estabelecimento</Card.Title>
+                        <Card.Text>
+                            <p> 
+                                <svg width="12" height="20" viewBox="0 0 18 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 12.35C8.14752 12.35 7.32995 12.0076 6.72716 11.3981C6.12436 10.7886 5.78571 9.96195 5.78571 9.1C5.78571 8.23805 6.12436 7.4114 6.72716 6.8019C7.32995 6.19241 8.14752 5.85 9 5.85C9.85248 5.85 10.67 6.19241 11.2728 6.8019C11.8756 7.4114 12.2143 8.23805 12.2143 9.1C12.2143 9.5268 12.1311 9.94941 11.9696 10.3437C11.8081 10.738 11.5713 11.0963 11.2728 11.3981C10.9744 11.6999 10.62 11.9393 10.2301 12.1026C9.84008 12.2659 9.42211 12.35 9 12.35ZM9 0C6.61305 0 4.32387 0.958747 2.63604 2.66533C0.948211 4.37191 0 6.68653 0 9.1C0 15.925 9 26 9 26C9 26 18 15.925 18 9.1C18 6.68653 17.0518 4.37191 15.364 2.66533C13.6761 0.958747 11.3869 0 9 0Z" fill="#545776"/></svg>
+                                A 983m do centro.
+                            </p>
                             <li>Categoria</li>
-                            <li>Nome</li>
-                            <li>Localização</li>
-                            <li>Descrição</li>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ex quis diam bibendum aliquet vel non tellus.</p>
                         </Card.Text>
                         <Button variant="primary">Visitar</Button>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src="holder.js/100px160" />
@@ -33,9 +41,6 @@ function Home() {
                         </Card.Text>
                         <Button variant="primary">Visitar</Button>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src="holder.js/100px160" />
@@ -49,59 +54,6 @@ function Home() {
                         </Card.Text>
                         <Button variant="primary">Visitar</Button>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
-            <CardGroup>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            <li>Categoria</li>
-                            <li>Nome</li>
-                            <li>Localização</li>
-                            <li>Descrição</li>
-                        </Card.Text>
-                        <Button variant="primary">Visitar</Button>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            <li>Categoria</li>
-                            <li>Nome</li>
-                            <li>Localização</li>
-                            <li>Descrição</li>
-                        </Card.Text>
-                        <Button variant="primary">Visitar</Button>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            <li>Categoria</li>
-                            <li>Nome</li>
-                            <li>Localização</li>
-                            <li>Descrição</li>
-                        </Card.Text>
-                        <Button variant="primary">Visitar</Button>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
             </CardGroup>
             <CardGroup>
@@ -117,9 +69,6 @@ function Home() {
                         </Card.Text>
                         <Button variant="primary">Visitar</Button>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src="holder.js/100px160" />
@@ -133,9 +82,6 @@ function Home() {
                         </Card.Text>
                         <Button variant="primary">Visitar</Button>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
                 </Card>
                 <Card>
                     <Card.Img variant="top" src="holder.js/100px160" />
@@ -149,9 +95,47 @@ function Home() {
                         </Card.Text>
                         <Button variant="primary">Visitar</Button>
                     </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
+                </Card>
+            </CardGroup>
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            <li>Categoria</li>
+                            <li>Nome</li>
+                            <li>Localização</li>
+                            <li>Descrição</li>
+                        </Card.Text>
+                        <Button variant="primary">Visitar</Button>
+                    </Card.Body>
                 </Card>
             </CardGroup>
         </>
